c3967677ed29d9b04b9d26db4847a3c1f00279de
Nicholas Christian <nicholasdchristian@gmail.com>
2023-02-26 13:59:52 -0500

    style: format terraform

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "this" {
 
 resource "aws_s3_bucket_acl" "example_bucket_acl" {
   bucket = aws_s3_bucket.this.id
-  acl = "log-delivery-write"
+  acl    = "log-delivery-write"
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
@@ -20,9 +20,9 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
 
-  block_public_acls = true
-  block_public_policy = true
-  ignore_public_acls = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
@@ -33,7 +33,7 @@ resource "aws_s3_bucket_notification" "this" {
   topic {
     topic_arn = aws_sns_topic.this.arn
     events = [
-      "s3:ObjectRemoved:*"]
+    "s3:ObjectRemoved:*"]
   }
 }
 
@@ -42,32 +42,32 @@ resource "aws_sns_topic" "this" {
 }
 
 resource "aws_sns_topic_policy" "this" {
-  arn = aws_sns_topic.this.arn
+  arn    = aws_sns_topic.this.arn
   policy = data.aws_iam_policy_document.this.json
 }
 
 
 data "aws_iam_policy_document" "this" {
   statement {
-    sid = "TopicPubishFromS3"
+    sid    = "TopicPubishFromS3"
     effect = "Allow"
 
     principals {
       type = "Service"
       identifiers = [
-        "s3.amazonaws.com"]
+      "s3.amazonaws.com"]
     }
     actions = [
       "SNS:Publish"
     ]
     resources = [
-      aws_sns_topic.this.arn]
+    aws_sns_topic.this.arn]
 
     condition {
-      test = "ForAnyValue:ArnEquals"
+      test     = "ForAnyValue:ArnEquals"
       variable = "aws:SourceArn"
       values = [
-        aws_s3_bucket.this.arn]
+      aws_s3_bucket.this.arn]
     }
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "application_name" {
-    description = "Name of the application. Used for identifying resources and tagging."
-    type = string
+  description = "Name of the application. Used for identifying resources and tagging."
+  type        = string
 }
